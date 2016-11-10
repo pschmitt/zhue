9f3928f21dfcb7a9a973578b037fc0c7c57952ae
Philipp Schmitt <philipp@schmitt.co>
2016-11-10 17:25:58 +0100

    Encapsulate light state into its own dedicated class

--- zhue/model/light.py
+++ zhue/model/light.py
@@ -6,6 +6,10 @@ class Light(hueobject.HueDevice):
     def __init__(self, *args, **kwargs):
         super(Light, self).__init__('lights', *args, **kwargs)
 
+    @property
+    def state(self):
+        return LightState(self._json['state'])
+
     def __on_off(self, state):
         assert state in ['on', 'off'], 'Unknown state: {}'.format(state)
         return self._set_state(data={'on': state == 'on'})
@@ -17,7 +21,49 @@ class Light(hueobject.HueDevice):
         return self.__on_off('off')
 
     def is_on(self):
-        return self.state['on']
+        return self.state.on
 
     def delete(self):
         return self._request(method='DELETE')
+
+
+class LightState(hueobject.HueObject):
+    @property
+    def alert(self):
+        return self._json['alert']
+
+    @property
+    def on(self):
+        return self._json['on']
+
+    @property
+    def brightness(self):
+        return self._json['bri']
+
+    @property
+    def colormode(self):
+        return self._json['colormode']
+
+    @property
+    def ct(self):
+        return self._json['ct']
+
+    @property
+    def effect(self):
+        return self._json['effect']
+
+    @property
+    def hue(self):
+        return self._json['hue']
+
+    @property
+    def reachable(self):
+        return self._json['reachable']
+
+    @property
+    def saturation(self):
+        return self._json['sat']
+
+    @property
+    def xy(self):
+        return self._json['xy']
