e10db2aaa038a36d68d6ebdb72b58accd38621fe
Philipp Schmitt <philipp@schmitt.co>
2016-11-14 15:12:06 +0100

    Add support for presence sensors and add new properties (mac_address and device_id)

--- zhue/model/sensor.py
+++ zhue/model/sensor.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 import hueobject
+import re
 
 
 def factory(bridge, hue_id, json):
@@ -7,6 +8,8 @@ def factory(bridge, hue_id, json):
         return TemperatureSensor(bridge, hue_id, json)
     elif json['type'] == 'ZLLLightLevel':
         return LightLevelSensor(bridge, hue_id, json)
+    elif json['type'] == 'ZLLPresence':
+        return PresenceSensor(bridge, hue_id, json)
     elif json['type'] == 'ZLLSwitch':
         return SwitchSensor(bridge, hue_id, json)
     else:
@@ -25,11 +28,39 @@ class Sensor(hueobject.HueDevice):
     def state(self):
         return SensorState(self._json['state'])
 
+    @property
+    def battery(self):
+        return self.config.battery
+
+    @property
+    def has_battery(self):
+        return self.battery is not None
+
+    def __decompose_uuid(self):
+        # FIXME Not all sensor types have an UUID
+        m = re.match(
+            r'(([0-9a-f]{2}[:-]){7}([0-9a-f]{2}))-([0-9a-f]{2}-[0-9a-f]{4})',
+            self.uuid
+        )
+        if m:
+            return m.group(1), m.group(4)
+        return None, None
+
+    @property
+    def mac_address(self):
+        mac, _ = self.__decompose_uuid()
+        return mac
+
+    @property
+    def device_id(self):
+        _, dev_id = self.__decompose_uuid()
+        return dev_id
+
 
 class SensorConfig(hueobject.HueObject):
     @property
     def battery(self):
-        return self._json['battery']
+        return self._json.get('battery', None)
 
 
 class SensorState(hueobject.HueObject):
@@ -116,6 +147,34 @@ class TemperatureSensorState(SensorState):
         return float(self._json['temperature'] / 100.0)
 
 
+class PresenceSensor(Sensor):
+    @property
+    def config(self):
+        return PresenceSensorConfig(self._json['config'])
+
+    @property
+    def state(self):
+        return PresenceSensorState(self._json['state'])
+
+    @property
+    def presence(self):
+        return self.state.presence
+
+    @property
+    def battery(self):
+        return self.config.battery
+
+
+class PresenceSensorConfig(SensorConfig):
+    pass
+
+
+class PresenceSensorState(SensorState):
+    @property
+    def presence(self):
+        return self._json['presence']
+
+
 class SwitchSensor(Sensor):
     @property
     def config(self):
@@ -123,7 +182,6 @@ class SwitchSensor(Sensor):
 
 
 class SwitchSensorConfig(SensorConfig):
-
     @property
     def on(self):
         return self._json['on']
