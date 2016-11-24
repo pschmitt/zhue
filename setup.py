99d9c516b25e9105d0146edc965b5aa62ff3a7c7
Philipp Schmitt <philipp@schmitt.co>
2016-11-24 18:28:24 +0100

    version 0.6.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='zhue',
-    version='0.5.0',
+    version='0.6.0',
     license='GPL3',
     description='Python SDK for Philips Hue devices',
     long_description=open('README.rst').read(),
