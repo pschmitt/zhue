1c66cd8ce93afcb087b4897cfd4f1e7ed8d6961c
Philipp Schmitt <philipp@schmitt.co>
2016-11-24 20:09:24 +0100

    Implement a bunch of Schedule properties

--- zhue/model/schedule.py
+++ zhue/model/schedule.py
@@ -7,3 +7,47 @@ import basemodel
 class Schedule(basemodel.HueLLDevice):
     def __init__(self, *args, **kwargs):
         super(Schedule, self).__init__('schedule', *args, **kwargs)
+
+    @property
+    def description(self):
+        return self._json['description']
+
+    @property
+    def created(self):
+        return self._json['created']
+
+    @property
+    def time(self):
+        return self._json['time']
+
+    @property
+    def localtime(self):
+        return self._json['localtime']
+
+    @property
+    def command(self):
+        return self._json['command']
+
+    @property
+    def method(self):
+        return self._json['command']['method']
+
+    @property
+    def body(self):
+        return self._json['command']['body']
+
+    @property
+    def device_address(self):
+        return self._json['command']['address']
+
+    @property
+    def device(self):
+        return self._bridge.from_address(self.device_address)
+
+    @property
+    def status(self):
+        return self._json['status']
+
+    @property
+    def recycle(self):
+        return self._json['recycle']
