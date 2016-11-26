c5a2cf4fff6f66c93121095a03038415d54b4e84
Philipp Schmitt <philipp@schmitt.co>
2016-11-26 12:25:05 +0100

    Implement Scene properties

--- zhue/model/scene.py
+++ zhue/model/scene.py
@@ -5,3 +5,31 @@ import basemodel
 class Scene(basemodel.BaseGroup):
     def __init__(self, *args, **kwargs):
         super(Scene, self).__init__('scenes', *args, **kwargs)
+
+    @property
+    def owner(self):
+        return self._bridge.user(username=self._json['owner'])
+
+    @property
+    def version(self):
+        return self._json['version']
+
+    @property
+    def picture(self):
+        return self._json['picture']
+
+    @property
+    def locked(self):
+        return self._json['locked']
+
+    @property
+    def last_updated(self):
+        return self._json['last_updated']
+
+    @property
+    def appdata(self):
+        return self._json['appdata']
+
+    @property
+    def recycle(self):
+        return self._json['recycle']
